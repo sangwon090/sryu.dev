1d85733dd957b8693bc6a1e304c931994e05f0d3
Sangwon Ryu <bdboy090@naver.com>
2026-02-02 15:33:44 +0900

    Upload resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://sryu.dev',
+  base: '/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [icon()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ko'],
+    routing: 'manual',
+  }
+});
